bfaf1e81d8c4be026fe7a774dfa4471cb3a52d20
Melloetta <truon370@umn.edu>
2022-11-03 22:48:34 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+// create a next js page with my name in the middle
+const Julia = () => {
+  return (
+    <div>
+      <h1 style = {{
+        textAlign: "center",
+        backgroundColor: "pink",
+        margin: "33px",
+        padding: "3px",
+      }}>Julia</h1>
+      <a href={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}>
+        <img src = {"/anya crying.jpg"} style = {{
+          display: "block",
+          marginLeft: "auto",
+          marginRight: "auto",
+          width: "50%",
+        }}/>
+      </a>
+    </div>
+  )
+}
+
+export default Julia;
